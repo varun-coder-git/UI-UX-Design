951526e0fd3b96142665796d7efc83a81033ebea
Varun Gupta <vrngupta878@gmail.com>
2022-09-04 23:41:44 +0530

    Commiting latest code

---
+++ src/app/app.module.ts
@@ -0,0 +1,72 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+// For MDB Angular Free
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDividerModule } from '@angular/material/divider';
+import { DatePipe } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatListModule} from '@angular/material/list'; 
+import {MatCardModule} from '@angular/material/card';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    BrowserModule,
+    MatBadgeModule,
+    AppRoutingModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatButtonModule,MatCheckboxModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    HttpClientModule,
+    FormsModule,
+    MatTabsModule,
+    MatChipsModule,
+   ReactiveFormsModule,
+    MatIconModule,
+    MatDialogModule,
+     MatTableModule,
+    MatPaginatorModule,MatCardModule,
+    MatSelectModule,MatSortModule,
+    MatSortModule, MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, 
+    MatInputModule, MatRadioModule, MatDividerModule,MatIconModule,
+
+  ],
+  providers: [DatePipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
