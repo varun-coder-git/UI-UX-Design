66cd1b43869a4bf813c74a08d66c7bee5ce03466
Varun Gupta <vrngupta878@gmail.com>
2022-09-08 16:26:41 +0530

    Latest code committed

---
+++ src/app/angular-charts/angular-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-charts',
+  templateUrl: './angular-charts.component.html',
+  styleUrls: ['./angular-charts.component.css']
+})
+export class AngularChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
